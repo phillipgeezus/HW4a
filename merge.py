96545bbd62ae91df76186eef55ed74392958d84f
Phillip Gee <geezus@Phillips-MacBook-Pro.local>
2023-03-20 23:21:20 -0700

    no.2

---
+++ merge.py
@@ -0,0 +1,15 @@
+def merge(list1, list2):
+    j = 0;
+    j = 0;
+    merged = []
+    
+    while i <len(list1) and j<len(list2):
+	if list1[i] <= list2[j]:
+	   merged.append(list1[i])
+	   i += 1
+	end:
+	   merged.append(list2[j:])
+	   j += 1
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+    return merged
