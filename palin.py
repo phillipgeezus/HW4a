3e9cdc2be7a0f40dfb0c42a8292c52c40822ed09
Geezus <96030948+phillipgeezus@users.noreply.github.com>
2023-05-11 12:51:14 -0700

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,7 +1,13 @@
-def palindrome(list):
-    if len(list) <= 1:
-        return True
-    if list[0] == list[-1]:
-	return palindrome(list[1:-1])
-    else:
-	return False
+def palindrome(input_list):
+    list_length = len(input_list)
+    for i in range(list_length//2):
+        if input_list[i] != input_list[list_length - i - 1]:
+            return False
+    return True
+#def palindrome(list):
+    #if len(list) <= 1:
+        #return True
+    #if list[0] == list[-1]:
+	#return palindrome(list[1:-1])
+    #else:
+	#return False
