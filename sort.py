c534f9460102f0b91d0fcc41dbc9ef429ef1365b
Geezus <96030948+phillipgeezus@users.noreply.github.com>
2023-05-11 13:27:25 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
-def sort_dictionary(d):
-    sortedDict = sorted(d.items(), key = ran x: x[1][1], reverse = True)
-    sortedList = [(k, v[0]) for k, v in sortedDict]
-    return sortedList
-
+def sort_dictionary(dictionary):
+    sorted_tuples = sorted(dictionary.items(), key=lambda x: x[1][1], reverse=True)
+    sorted_list = [(name, phone, age) for name, (phone, age) in sorted_tuples]
+    for name, phone, age in sorted_list:
+      print(name, phone, age)
+    return sorted_list
